51ea2e3452e7cbdf6a75ba1793d7fbc8b08ce4b0
damianhunt <dhuntsa@gmail.com>
2022-06-16 19:13:19 +0200

    commit

--- snake-incomplete/snake-incomplete/snake/game/scripting/move_actors_action.py
+++ snake-incomplete/snake-incomplete/snake/game/scripting/move_actors_action.py
@@ -7,3 +7,12 @@ from game.scripting.action import Action
 # 1) get all the actors from the cast
 # 2) loop through the actors
 # 3) call the move_next() method on each actor
+
+class MoveActorsAction(Action):
+
+    def execute(self, cast, script):
+
+        actors = cast.get_all_actors()
+        for actor in actors:
+            actor.move_next()
+
